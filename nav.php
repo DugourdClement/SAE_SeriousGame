f40104afeca4325c4ede96b7c714d7763998f8aa
TheGreatDuke <92146571+DugourdClement@users.noreply.github.com>
2023-01-16 19:41:56 +0100

    Update chemins

--- nav.php
+++ nav.php
@@ -15,8 +15,8 @@ echo '
 <div id="mySidenav" class="sidenav">
     <a id="closeBtn" href="#" class="close">×</a>
     <ul>
-        <li><a onclick="closeNav()" href="index.php#ressource">Ressources</a></li>
-        <li><a onclick="closeNav()" href="#index.php#cartes">Contacts</a></li>
+        <li><a onclick="closeNav()" href="#ressource">Ressources</a></li>
+        <li><a onclick="closeNav()" href="#cartes">Contacts</a></li>
         <li><a onclick="closeNav()" href="chatbot.php" target="_blank">Chatbot</a></li>';
 login();
 echo '
@@ -36,8 +36,8 @@ echo '
         <div class="logo"><a href="index.php"><img id="logoManette" src="Picture/site/logosae.png" alt=" retour en haut "></a></div>
         <div id="mainListDiv" class="main_list">
             <ul class="navlinks">
-                <li><a href="index.php#ressource">Ressources</a></li>
-                <li><a href="index.php#cartes">Contacts</a></li>
+                <li><a href="#ressource">Ressources</a></li>
+                <li><a href="#cartes">Contacts</a></li>
                 <li><a href="chatbot.php" target="_blank">ChatBox</a></li>';
 login();
 echo '
