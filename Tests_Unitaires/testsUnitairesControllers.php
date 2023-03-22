22784f7e16585ca8665c5bad707300606ab458fa
Enzo <you@example.commekerkenzo@gmail.com>
2023-03-22 21:30:32 +0100

    Modifications tests unitaires

--- Tests_Unitaires/testsUnitairesControllers.php
+++ Tests_Unitaires/testsUnitairesControllers.php
@@ -5,28 +5,33 @@ class testsUnitairesControllers extends TestCase
 {
     public function testModificationAction()
     {
-        $login = 'testuser';
-        $password = 'testpassword';
-        $data = array('champ1' => 'valeur1', 'champ2' => 'valeur2');
-        $modificationForm = new ModificationForm();
+        // Créer un objet de la classe Controllers
+        $controllers = new Controllers();
 
-        $test = new Controllers();
-
-        $modificationFormMock = $this->getMockBuilder(ModificationForm::class)
-            ->setMethods(array('authenticate', 'getAllForm'))
+        // Simuler les données nécessaires pour l'exécution de la fonction
+        $login = 'john_doe';
+        $password = 'password123';
+        $data = ['name' => 'John Doe', 'email' => 'john@example.com'];
+        $modificationForm = $this->getMockBuilder(ModificationForm::class)
+            ->disableOriginalConstructor()
             ->getMock();
-        $modificationFormMock->expects($this->once())
+
+        // Définir les attentes pour la méthode authenticate() du Mock de ModificationForm
+        $modificationForm->expects($this->once())
             ->method('authenticate')
             ->with($this->equalTo($login), $this->equalTo($password), $this->equalTo($data))
             ->willReturn(true);
 
-        $modificationFormMock->expects($this->once())
+        // Exécuter la fonction à tester
+        $controllers->modificationAction($login, $password, $data, $modificationForm);
+
+        // Vérifier que les sessions ont été initialisées correctement
+        $this->assertEquals($login, $_SESSION['username']);
+        $this->assertTrue($_SESSION['isLogin']);
+
+        // Vérifier que la méthode getAllForm() de $modificationForm a été appelée une fois avec $data en argument
+        $modificationForm->expects($this->once())
             ->method('getAllForm')
             ->with($this->equalTo($data));
-
-        $example->modificationAction($login, $password, $data, $modificationFormMock);
-        
-        $this->assertEquals($_SESSION['username'], $login);
-        $this->assertEquals($_SESSION['isLogin'], true);
     }
 }
--- Tests_Unitaires/testsUnitairesYearData.php
+++ Tests_Unitaires/testsUnitairesYearData.php
@@ -3,70 +3,27 @@ use PHPUnit\Framework\TestCase;
 
 class testsUnitairesYearData extends TestCase
 {
-    public function testConstruct()
+
+    public function testCanBeCreatedFromValidData(): void
     {
         $year = 2022;
-        $nbTextSup = 2;
-        $textSup = array('Texte supplémentaire 1', 'Texte supplémentaire 2');
+        $nbTextSup = 5;
+        $textSup = ['text1', 'text2', 'text3'];
         $nbChoices = 3;
-        $choices = array('Choix 1', 'Choix 2', 'Choix 3');
+        $choices = [
+            new Choice('choice1', 2, ['option1', 'option2']),
+            new Choice('choice2', 3, ['option1', 'option2', 'option3']),
+            new Choice('choice3', 1, ['option1']),
+        ];
 
-        $test = new YearData($year, $nbTextSup, $textSup, $nbChoices, $choices);
-    
+        $yearData = new YearData($year, $nbTextSup, $textSup, $nbChoices, $choices);
 
-        $this->assertEquals($year, $test->year);
-        $this->assertEquals($nbTextSup, $test->nbTextSup);
-        $this->assertEquals($textSup, $test->textSup);
-        $this->assertEquals($nbChoices, $test->nbChoices);
-        $this->assertEquals($choices, $test->choices);
-    }
-
-    public function testGetYear()
-    {
-        $test = new YearData(2022, 2, array('Texte supplémentaire 1', 'Texte supplémentaire 2'), 3, array('Choix 1', 'Choix 2', 'Choix 3'));
-
-        $this->assertEquals(2022, $test->getYear());
-    }
-
-    public function testGetNbTextSup()
-    {
-        $test = new YearData(2022, 2, array('Texte supplémentaire 1', 'Texte supplémentaire 2'), 3, array('Choix 1', 'Choix 2', 'Choix 3'));
-
-        $this->assertEquals(2, $test->getNbTextSup());
-    }
-
-    public function testGetTextSup()
-    {
-        $test = new YearData(2022, 2, array('Texte supplémentaire 1', 'Texte supplémentaire 2'), 3, array('Choix 1', 'Choix 2', 'Choix 3'));
-
-        $this->assertEquals(array('Texte supplémentaire 1', 'Texte supplémentaire 2'), $test->getTextSup());
-    }
-
-    public function testGetNbChoices()
-    {
-        $test = new YearData(2022, 2, array('Texte supplémentaire 1', 'Texte supplémentaire 2'), 3, array('Choix 1', 'Choix 2', 'Choix 3'));
-
-        $this->assertEquals(3, $test->getNbChoices());
-    }
-
-    public function testGetChoices()
-    {
-        $test = new YearData(2022, 2, array('Texte supplémentaire 1', 'Texte supplémentaire 2'), 3, array('Choix 1', 'Choix 2', 'Choix 3'));
-
-        $this->assertEquals(array('Choix 1', 'Choix 2', 'Choix 3'), $test->getChoices());
-    }
-
-    public function testJsonSerialize()
-    {
-        $test = new YearData(2022, 2, array('Texte supplémentaire 1', 'Texte supplémentaire 2'), 3, array('Choix 1', 'Choix 2', 'Choix 3'));
-
-        $json = $test->jsonSerialize();
-
-        $this->assertEquals(2022, $json['year']);
-        $this->assertEquals(2, $json['nbTextSup']);
-        $this->assertEquals(array('Texte supplémentaire 1', 'Texte supplémentaire 2'), $json['textSup']);
-        $this->assertEquals(3, $json['nbChoice']);
-        $this->assertEquals(array('Choix 1', 'Choix 2', 'Choix 3'), $json['choice']);
+        $this->assertInstanceOf(YearData::class, $yearData);
+        $this->assertEquals($year, $yearData->getYear());
+        $this->assertEquals($nbTextSup, $yearData->getNbTextSup());
+        $this->assertEquals($textSup, $yearData->getTextSup());
+        $this->assertEquals($nbChoices, $yearData->getNbChoices());
+        $this->assertEquals($choices, $yearData->getChoices());
     }
 
 }
