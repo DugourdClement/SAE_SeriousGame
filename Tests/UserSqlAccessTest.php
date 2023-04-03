2373670478bd9bd96e78c26956c67ce66a829587
Enzo MEKERKE <92160495+EnzoMEKERKE@users.noreply.github.com>
2023-04-03 20:54:49 +0200

    Update UserSqlAccessTest.php

--- Tests/UserSqlAccessTest.php
+++ Tests/UserSqlAccessTest.php
@@ -14,10 +14,8 @@ $this->dataAccess = $this->createMock(UserSqlAccess::class);
 
     public function testIsUser()
     {
-        // Create instance of UserSqlAccess
         $userSqlAccess = new UserSqlAccess($this->dataAccess);
 
-        // Set expectations for PDO mock object
         $this->dataAccess->expects($this->once())
             ->method('prepare')
             ->with($this->equalTo("SELECT mdp FROM utilisateur WHERE identifiant = :username"))
@@ -35,21 +33,16 @@ $this->dataAccess = $this->createMock(UserSqlAccess::class);
             ->method('lastInsertId')
             ->willReturn('1');
 
-        // Call method being tested
         $result = $userSqlAccess->isUser('testuser', 'testpassword');
 
-        // Assert that the result matches the expected value
         $this->assertTrue($result);
     }
 public function testVerifyCaptcha()
 {
-// Create instance of UserSqlAccess
 $userSqlAccess = new UserSqlAccess($this->dataAccess);
 
-// Call method being tested
 $result = $userSqlAccess->verifyCaptcha('testresponse');
 
-// Assert that the result matches the expected value
 $this->assertFalse($result);
 }
 }
